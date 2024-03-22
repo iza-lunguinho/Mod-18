0c0d56bd43e09bfb47390ddbcae85e5d617c932d
iza <izabellalunguinho00@gmail.com>
2024-03-22 19:23:04 -0300

    first commit

---
+++ projeto18.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Carregando os dados do arquivo gasolina.csv
+df = pd.read_csv('gasolina.csv')
+
+# Criando o gráfico de linha
+plt.figure(figsize=(10, 6))
+sns.lineplot(data=df, x='dia', y='venda', marker='o')
+
+# Adicionando título e rótulos dos eixos
+plt.title('Preço da Gasolina ao Longo do Tempo')
+plt.xlabel('Dia')
+plt.ylabel('Preço (R$)')
+
+# Salvando o gráfico como gasolina.png
+plt.savefig('gasolina.png')
+
+# Exibindo o gráfico
+plt.show()
